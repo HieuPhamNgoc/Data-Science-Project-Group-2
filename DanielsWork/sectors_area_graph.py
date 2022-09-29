83a327b4bd8e487022f164bd75af142c3d17107b
Daniel <daniel.aaltonen@aalto.fi>
2022-09-29 19:38:34 +0300

    modified margins, changed template

--- DanielsWork/sectors_area_graph.py
+++ DanielsWork/sectors_area_graph.py
@@ -1,4 +1,3 @@
-from re import T
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
@@ -8,7 +7,7 @@ import pandas as pd
 
 raw_data = pd.read_excel(io="DanielsWork\EDGARv7.0_FT2021_fossil_CO2_booklet_2022.xlsx", sheet_name="fossil_CO2_by_sector_and_countr")
 raw_data.drop(columns=["Substance", "EDGAR Country Code"], inplace=True) # drop unnecessary columns
-scope = [*set(raw_data["Country"])]; scope.append("Global emissions") # selectable countries + GLOBAL VIEW
+scope = sorted([*set(raw_data["Country"])]); scope.append("Global emissions") # selectable countries + GLOBAL VIEW
 raw_data.fillna(0, inplace=True) # replace unreported emissions with 0
 
 app = Dash(__name__)
@@ -23,7 +22,7 @@ app.layout = html.Div(
                         options=scope,
                         multi=False,
                         value = 'Global emissions',
-                        style={'width':'40%'})
+                        style={'width':'60%'})
             ],
             style={'width': '50%', 'margin-left': '50px'}
         ),
@@ -45,14 +44,16 @@ def update_graph(selection):
         data.columns = data.columns.astype(str) # change names to string to ensure sound processing
         # we need the data from wide-form to long-form (see internet or plotly docs on why):
         data = pd.melt(data, id_vars = "Sector", value_vars=data.columns, var_name="Year", value_name = "CO2 emissions", ignore_index=True)
-        fig = px.area(data, x="Year", y="CO2 emissions", color="Sector")
+        fig = px.area(data, x="Year", y="CO2 emissions", color="Sector", template="none")
     else:
+        # dont know why this slightly bugs out. Python thinks selecting row based on column criteria
+        # produces a series when it produces a dataframe. Works when running so no problem
         data = data[data["Country"] == selection] # select country data
         data.drop(columns=["Country"], inplace=True)
         data.columns = data.columns.astype(str) # change names to string to ensure sound processing
         # we need the data from wide-form to long-form (see internet or plotly docs on why):
         data = pd.melt(data, id_vars = "Sector", value_vars=data.columns[1:], var_name="Year", value_name = "CO2 emissions", ignore_index=True)
-        fig = px.area(data, x="Year", y="CO2 emissions", color="Sector")
+        fig = px.area(data, x="Year", y="CO2 emissions", color="Sector", template="none")
     return fig
 
 if __name__ == '__main__':
