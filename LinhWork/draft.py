1c23817d7f9ed9f4df98872f8da1fce0e3cea669
linhlngo <110495942+linhlngo@users.noreply.github.com>
2022-10-03 14:50:24 +0300

    Add files via upload

--- LinhWork/draft.py
+++ LinhWork/draft.py
@@ -1,134 +1,92 @@
+import pandas as pd
+import dash
 
-df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+from dash import html, dcc, Dash
+from dash.dependencies import Input, Output
+import plotly.express as px
 
 
-app.layout = html.Div([
-    html.Div([
+# Preprocessing data
+df_co2 = pd.read_excel('co2_by_country.xlsx')
+df_co2 = df_co2.drop(columns=['Substance', 'EDGAR Country Code'])
+df_co2 = df_co2.drop(columns= df_co2.columns[1:21])
+scope = list(df_co2["Country"].unique())
 
-        html.Div([
-            dcc.Dropdown(
-                df['Indicator Name'].unique(),
-                'Fertility rate, total (births per woman)',
-                id='crossfilter-xaxis-column',
-            ),
-            dcc.RadioItems(
-                ['Linear', 'Log'],
-                'Linear',
-                id='crossfilter-xaxis-type',
-                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
-            )
-        ],
-        style={'width': '49%', 'display': 'inline-block'}),
+#df_co2 = df_co2.reset_index(drop=True).set_index('Country')
+#df_co2 = pd.melt(df_co2, id_vars='Country', value_vars= df_co2.columns[1:],  var_name='Year', value_name='Change in CO2 emissions')
 
-        html.Div([
-            dcc.Dropdown(
-                df['Indicator Name'].unique(),
-                'Life expectancy at birth, total (years)',
-                id='crossfilter-yaxis-column'
-            ),
-            dcc.RadioItems(
-                ['Linear', 'Log'],
-                'Linear',
-                id='crossfilter-yaxis-type',
-                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
-            )
-        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
-    ], style={
-        'padding': '10px 5px'
-    }),
+df = df_co2.copy()
+df = pd.melt(df, id_vars='Country', value_vars= df.columns[1:],  var_name='Year', value_name='Change in CO2 emissions')
+year = set(df.Year.unique())
 
-    html.Div([
-        dcc.Graph(
-            id='crossfilter-indicator-scatter',
-            hoverData={'points': [{'customdata': 'Japan'}]}
+# df_GDP = pd.read_excel('GDP_by_country_current_international_dollar.xlsx', skiprows = 3)
+# df_GDP = df_GDP.drop(columns= df_GDP.columns[1:34])
+# df_GDP = df_GDP.reset_index(drop=True).set_index('Country Name')
+# df_GDP = df_GDP.pct_change(axis=1).fillna(0)
+
+
+# df.iloc[:, 1:] = (df.iloc[:, 1:] - df.iloc[0, 1:].values.squeeze())
+# .div(df.iloc[:, 1:])
+
+
+
+app = Dash(__name__)
+
+
+app.layout = html.Div(
+    children = [
+        html.H1('Changes in CO2 emissions', style = {'text-align':'center'}),
+        html.Div(
+            children = [
+            html.H3('Select scope:'),
+            dcc.Dropdown(id = "scope",
+                        options=scope,
+                        multi=False,
+                        value = "Finland",
+                        style={'width':'60%'})
+            ],
+            style={'width': '50%', 'margin-left': '50px'}
+        ),
+        html.Br(),
+        dcc.Graph(id ='C02_change_emissions_graph', style = {'margin-left':'150px'}),
+        html.Br(),
+         html.Div(
+            children = [
+            html.H3('Select year:'),
+            dcc.RangeSlider( 
+                min = 1990, 
+                max = 2021,  
+                step = 1 ,
+                value = [1990,1995],
+                marks={int(i): '{}'.format(i) for i in range(1990,2022)},
+                id='my-range-slider')
+            ]
+                )
+            ], style={'width': '80%', 'margin-left': '50px'}
         )
-    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
-    html.Div([
-        dcc.Graph(id='x-time-series'),
-        dcc.Graph(id='y-time-series'),
-    ], style={'display': 'inline-block', 'width': '49%'}),
-
-    html.Div(dcc.Slider(
-        df['Year'].min(),
-        df['Year'].max(),
-        step=None,
-        id='crossfilter-year--slider',
-        value=df['Year'].max(),
-        marks={str(year): str(year) for year in df['Year'].unique()}
-    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
-])
-
 
 @app.callback(
-    Output('crossfilter-indicator-scatter', 'figure'),
-    Input('crossfilter-xaxis-column', 'value'),
-    Input('crossfilter-yaxis-column', 'value'),
-    Input('crossfilter-xaxis-type', 'value'),
-    Input('crossfilter-yaxis-type', 'value'),
-    Input('crossfilter-year--slider', 'value'))
-    
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,
-                 year_value):
-    dff = df[df['Year'] == year_value]
+    Output('C02_change_emissions_graph', "figure"), 
+    Input("scope", "value"),
+    [Input('my-range-slider', 'value')],
+    )
 
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name']
-            )
 
-    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
 
-    fig.update_xaxes(title=xaxis_column_name, type='linear' if xaxis_type == 'Linear' else 'log')
-
-    fig.update_yaxes(title=yaxis_column_name, type='linear' if yaxis_type == 'Linear' else 'log')
-
-    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
-
+def update_graph(scope_selected, years_selected):
+    data = df_co2.copy()
+    data = data[data["Country"] == scope_selected]
+    #data.iloc[:,1:] = data.iloc[:, 1:].pct_change(axis=1).fillna(0)
+    data.columns = data.columns.astype(str) 
+    data = pd.melt(data, id_vars='Country', value_vars= data.columns[1:],  var_name='Year', value_name='Change', ignore_index=True)
+    data['Year'] = data['Year'].astype('int')
+    data = data[(data["Year"] >= years_selected[0]) & (data["Year"] <= years_selected[1])]
+    data.drop(columns=["Country"], inplace=True)
+    #data['Change'] = data.iloc[1:,:].apply(lambda row: row)
+    #data.iloc[1:,1] = ( data.iloc[1:,1] - data.iloc[0,1].values.squeeze())
+    data.iloc[:,1] = data.iloc[:,1].apply(lambda row : (row - data.iloc[0,1]) /  data.iloc[0,1])
+    fig = px.line(data, x= "Year", y="Change", template="none")
     return fig
-
-
-def create_time_series(dff, axis_type, title):
-
-    fig = px.scatter(dff, x='Year', y='Value')
-
-    fig.update_traces(mode='lines+markers')
-
-    fig.update_xaxes(showgrid=False)
-
-    fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
-
-    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
-                       xref='paper', yref='paper', showarrow=False, align='left',
-                       text=title)
-
-    fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
-
-    return fig
-
-
-@app.callback(
-    Output('x-time-series', 'figure'),
-    Input('crossfilter-indicator-scatter', 'hoverData'),
-    Input('crossfilter-xaxis-column', 'value'),
-    Input('crossfilter-xaxis-type', 'value'))
-def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
-    country_name = hoverData['points'][0]['customdata']
-    dff = df[df['Country Name'] == country_name]
-    dff = dff[dff['Indicator Name'] == xaxis_column_name]
-    title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
-    return create_time_series(dff, axis_type, title)
-
-
-@app.callback(
-    Output('y-time-series', 'figure'),
-    Input('crossfilter-indicator-scatter', 'hoverData'),
-    Input('crossfilter-yaxis-column', 'value'),
-    Input('crossfilter-yaxis-type', 'value'))
-def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
-    dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
-    dff = dff[dff['Indicator Name'] == yaxis_column_name]
-    return create_time_series(dff, axis_type, yaxis_column_name)
 
 
 if __name__ == '__main__':
--- LinhWork/main.py
+++ LinhWork/main.py
@@ -11,17 +11,14 @@ df_co2 = pd.read_excel('co2_by_country.xlsx')
 df_co2 = df_co2.drop(columns=['Substance', 'EDGAR Country Code'])
 df_co2 = df_co2.drop(columns= df_co2.columns[1:21])
 scope = list(df_co2["Country"].unique())
-#df_co2 = df_co2.reset_index(drop=True).set_index('Country')
-#df_co2 = pd.melt(df_co2, id_vars='Country', value_vars= df_co2.columns[1:],  var_name='Year', value_name='Change in CO2 emissions')
 
 df = df_co2.copy()
 df = pd.melt(df, id_vars='Country', value_vars= df.columns[1:],  var_name='Year', value_name='Change in CO2 emissions')
-
 year = set(df.Year.unique())
-# df_GDP = pd.read_excel('GDP_by_country_current_international_dollar.xlsx', skiprows = 3)
-# df_GDP = df_GDP.drop(columns= df_GDP.columns[1:34])
-# df_GDP = df_GDP.reset_index(drop=True).set_index('Country Name')
-# df_GDP = df_GDP.pct_change(axis=1).fillna(0)
+
+df_GDP = pd.read_excel('GDP_by_country_current_international_dollar.xlsx', skiprows = 3)
+df_GDP = df_GDP.drop(columns= df_GDP.columns[1:34])
+
 
 
 # df.iloc[:, 1:] = (df.iloc[:, 1:] - df.iloc[0, 1:].values.squeeze())
@@ -34,7 +31,7 @@ app = Dash(__name__)
 
 app.layout = html.Div(
     children = [
-        html.H1('Changes in CO2 emissions', style = {'text-align':'center'}),
+        html.H1('Changes in CO2 emissions and GDP', style = {'text-align':'center'}),
         html.Div(
             children = [
             html.H3('Select scope:'),
@@ -56,7 +53,7 @@ app.layout = html.Div(
                 min = 1990, 
                 max = 2021,  
                 step = 1 ,
-                value = [1990,1995],
+                value = [2000, 2021],
                 marks={int(i): '{}'.format(i) for i in range(1990,2022)},
                 id='my-range-slider')
             ]
@@ -74,19 +71,31 @@ app.layout = html.Div(
 
 def update_graph(scope_selected, years_selected):
     data = df_co2.copy()
+    data1 = df_GDP.copy()
+    ###
     data = data[data["Country"] == scope_selected]
-    #data.iloc[:,1:] = data.iloc[:, 1:].pct_change(axis=1).fillna(0)
     data.columns = data.columns.astype(str) 
     data = pd.melt(data, id_vars='Country', value_vars= data.columns[1:],  var_name='Year', value_name='Change', ignore_index=True)
     data['Year'] = data['Year'].astype('int')
     data = data[(data["Year"] >= years_selected[0]) & (data["Year"] <= years_selected[1])]
     data.drop(columns=["Country"], inplace=True)
-    #data['Change'] = data.iloc[1:,:].apply(lambda row: row)
-    #data.iloc[1:,1] = ( data.iloc[1:,1] - data.iloc[0,1].values.squeeze())
-    data.iloc[:,1] = data.iloc[:,1].apply(lambda row : (((row - data.iloc[0,1]) /  data.iloc[0,1]) * 100.0))
-    
+    data.iloc[:,1] = data.iloc[:,1].apply(lambda row : (((row - data.iloc[0,1]) /  data.iloc[0,1]) ))
+    data["Metric"] = "CO2"
 
-    fig = px.line(data, x= "Year", y="Change", template="none")
+    ### 
+    data1 = data1[data1["Country Name"] == scope_selected]
+    data1.columns = data1.columns.astype(str)
+    data1 = pd.melt(data1, id_vars='Country Name', value_vars= data1.columns[1:],  var_name='Year', value_name='Change', ignore_index=True)
+    data1['Year'] = data1['Year'].astype('int')
+    data1 = data1[(data1["Year"] >= years_selected[0]) & (data1["Year"] <= years_selected[1])]
+    data1.drop(columns=["Country Name"], inplace=True)
+    data1.iloc[:,1] = data1.iloc[:,1].apply(lambda row : (((row - data1.iloc[0,1]) /  data1.iloc[0,1]) ))
+    data1["Metric"] = "GDP"
+    
+    df  = pd.concat([data,data1.loc[:]]).reset_index(drop=True)
+    fig = px.line(df, x= "Year", y="Change", color="Metric", template="none")
+    fig.layout.yaxis.tickformat = ',.1%'
+
     return fig
 
 
