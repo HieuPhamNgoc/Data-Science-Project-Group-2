162bb7aa8eccddf8002bb1b29d48171a040f560a
Daniel <daniel.aaltonen@aalto.fi>
2022-11-16 10:52:41 +0200

    further prediction work

--- DanielsWork/prediction.py
+++ DanielsWork/prediction.py
@@ -1,6 +1,5 @@
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
-import plotly.tools as tls
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 
@@ -14,10 +13,25 @@ co2_data = pd.melt(co2_data, id_vars="id", var_name="Year", value_name="Mt CO2")
 co2_data.drop(columns=["id"], inplace=True)
 co2_data.set_index("Year", inplace=True)
 
+# super simple AR model (once differenced)
 model = ARIMA(co2_data, order=(1,1,0))
 result = model.fit()
-pred = result.get_prediction(steps=5)
-fig = px.line(pred)
+# predict 5 years out
+mat_pred = result.get_prediction(start=1, end=56)
+# default plots by statsmodels in matplotlib WHICH DONT FUCKING WORK WITH DASH!!!
+pred_df = mat_pred.summary_frame()
+pred_df.drop(columns=["mean_se"], inplace=True)
+pred_df.columns = ["Prediction", "Lower CI (95%)", "Higher CI (95%)"]
+#pred_df.insert(0, {"Prediction": None, "Lower CI (95%)": None, "Higher CI (95%)": None})
+pred_df["Year"] = [year for year in range(1971,2027)]
+pred_df.set_index("Year", inplace=True)
+#pred_df.insert(1970, {"Prediction": None, "Lower CI (95%)": None, "Higher CI (95%)": None})
+print(pred_df)
+print(co2_data)
+'''
+plot_pred = fig_to_plotly(mat_pred)
+fig = px.line(plot_pred)
+
 
 app = Dash(__name__)
 
@@ -32,3 +46,4 @@ app.layout = html.Div(
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+'''
